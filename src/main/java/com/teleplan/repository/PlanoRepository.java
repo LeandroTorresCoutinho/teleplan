304fb332eb33fa5ac86bca489cb49d3ae533a513
Leandro Torres Coutinho <leandrotorrescoutinho@Leandros-MacBook-Pro.local>
2020-10-16 22:18:43 -0300

    criando repository para plano

---
+++ src/main/java/com/teleplan/repository/PlanoRepository.java
@@ -0,0 +1,9 @@
+package com.teleplan.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.teleplan.model.Plano;
+
+public interface PlanoRepository extends CrudRepository<Plano, Long>{
+
+}
