b5e6235b313f8ed08a028fe6edbbbcfe793d28d6
Leandro Torres Coutinho <leandrotorrescoutinho@Leandros-MacBook-Pro.local>
2020-10-17 09:52:02 -0300

    refatorando para receber Optional ao invez de list

--- src/main/java/com/teleplan/repository/PlanoRepository.java
+++ src/main/java/com/teleplan/repository/PlanoRepository.java
@@ -1,6 +1,7 @@
 package com.teleplan.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,9 +10,9 @@ import com.teleplan.model.Tipo;
 
 public interface PlanoRepository extends CrudRepository<Plano, Long>{
 
-	public List<Plano> findByCodigoDDD(String ddd);
-	public List<Plano> findByIdTipo(Tipo tipo);
-	public List<Plano> findByCodigoDDDAndIdTipoAllIgnoreCase(String ddd, Tipo tipo);
-	public List<Plano> findByCodigoDDDAndOperadoraAllIgnoreCase(String ddd, String operadora);
-	public List<Plano> findByCodigoDDDAndId(String ddd, long id);
+	public Optional<Plano> findByCodigoDDD(String ddd);
+	public Optional<Plano> findByIdTipo(Tipo tipo);
+	public Optional<Plano> findByCodigoDDDAndIdTipoAllIgnoreCase(String ddd, Tipo tipo);
+	public Optional<Plano> findByCodigoDDDAndOperadoraAllIgnoreCase(String ddd, String operadora);
+	public Optional<Plano> findByCodigoDDDAndId(String ddd, long id);
 }
