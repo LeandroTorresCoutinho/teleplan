65b8e8ca710df903551dd00a40fb57ea20ff26dd
Leandro Torres Coutinho <leandrotorrescoutinho@Leandros-MacBook-Pro.local>
2020-10-17 10:26:42 -0300

    adicionando outras buscas

--- src/main/java/com/teleplan/service/PlanoService.java
+++ src/main/java/com/teleplan/service/PlanoService.java
@@ -1,8 +1,7 @@
 package com.teleplan.service;
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import com.teleplan.model.Plano;
 import com.teleplan.model.Tipo;
@@ -10,6 +9,8 @@ import com.teleplan.model.Tipo;
 public interface PlanoService {
 	
 	public List<Plano> findByCodigoDDD(String ddd);
-	
 	public List<Plano> findByIdTipo(Tipo tipo);
+	public List<Plano> findByCodigoDDDAndIdTipoAllIgnoreCase(String ddd, Tipo tipo);
+	public List<Plano> findByCodigoDDDAndOperadoraAllIgnoreCase(String ddd, String operadora);
+	public Optional<Plano> findByCodigoDDDAndId(String ddd, long id);
 }
