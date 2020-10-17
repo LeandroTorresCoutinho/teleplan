b0d67ff73eca16eea889b12d8e929d9932a9bcf5
Leandro Torres Coutinho <leandrotorrescoutinho@Leandros-MacBook-Pro.local>
2020-10-17 09:45:50 -0300

    adicionando outras buscas

--- src/main/java/com/teleplan/repository/PlanoRepository.java
+++ src/main/java/com/teleplan/repository/PlanoRepository.java
@@ -10,6 +10,8 @@ import com.teleplan.model.Tipo;
 public interface PlanoRepository extends CrudRepository<Plano, Long>{
 
 	public List<Plano> findByCodigoDDD(String ddd);
-	
 	public List<Plano> findByIdTipo(Tipo tipo);
+	public List<Plano> findByCodigoDDDAndIdTipoAllIgnoreCase(String ddd, Tipo tipo);
+	public List<Plano> findByCodigoDDDAndOperadoraAllIgnoreCase(String ddd, String operadora);
+	public List<Plano> findByCodigoDDDAndId(String ddd, long id);
 }
