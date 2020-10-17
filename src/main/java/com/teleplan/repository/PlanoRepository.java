fb5c18cbe44ad315bdfbd3a6fcac90d0ffcf8e4b
Leandro Torres Coutinho <leandrotorrescoutinho@Leandros-MacBook-Pro.local>
2020-10-17 00:50:36 -0300

    including findByIdTipo

--- src/main/java/com/teleplan/repository/PlanoRepository.java
+++ src/main/java/com/teleplan/repository/PlanoRepository.java
@@ -1,9 +1,15 @@
 package com.teleplan.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.teleplan.model.Plano;
+import com.teleplan.model.Tipo;
 
 public interface PlanoRepository extends CrudRepository<Plano, Long>{
 
+	public List<Plano> findByCodigoDDD(String ddd);
+	
+	public List<Plano> findByIdTipo(Tipo tipo);
 }
