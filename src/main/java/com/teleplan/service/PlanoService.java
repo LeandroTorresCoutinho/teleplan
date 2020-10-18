1b311d4f0c5857661b785ab797795ad37864722d
Leandro Torres Coutinho <leandrotorrescoutinho@Leandros-MacBook-Pro.local>
2020-10-18 01:51:40 -0300

    adicionando save, update e delete metodos nos services

--- src/main/java/com/teleplan/service/PlanoService.java
+++ src/main/java/com/teleplan/service/PlanoService.java
@@ -8,6 +8,9 @@ import com.teleplan.model.Tipo;
 
 public interface PlanoService {
 	
+	public Plano save(String codigoDDD, int minutos, int franquiaDeInternet, double valor, String tipo, String operadora);
+	public Plano update(long id, String codigoDDD, int minutos, int franquiaDeInternet, double valor, String tipo, String operadora);
+	public void deleteById(long id);
 	public List<Plano> findByCodigoDDD(String ddd);
 	public List<Plano> findByTipo(Tipo tipo);
 	public List<Plano> findByCodigoDDDAndTipoAllIgnoreCase(String ddd, String tipo);
